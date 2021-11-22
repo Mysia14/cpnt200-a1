0910ecec71378cef38df66751f783a7f0da749e6
Mysia Santana <mysiasantana@gmail.com>
2021-11-22 14:47:50 -0700

    edited page paragraph

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
  <Header />
     <main>
       <p> Introdution to Nuxt and Vue</p>
-      <p> A cute Pom image as an ilustration for the assignment</p>
+      <p> A Fall imagage as an ilustration for the assignment</p>
       <nuxt-img src="/assets/images/fall.jpg" size="sm:25 px md:50 px lg:100px" />
     </main> 
 </div>
